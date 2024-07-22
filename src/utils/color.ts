8fcc591b36ee5aa7a2b5973e123443d24c2c6db3
Sivert Gullberg Hansen <root@sivert.io>
2024-07-22 22:36:17 +0200

    Updated colors

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -3,15 +3,15 @@ import { TextProps } from "@radix-ui/themes";
 export function getPlayerColor(rating: number): TextProps["color"] {
   if (rating < 4999) return "gray";
 
-  if (rating < 9999) return "cyan";
+  if (rating < 9999) return "bronze";
 
-  if (rating < 14999) return "blue";
+  if (rating < 14999) return "iris";
 
-  if (rating < 19999) return "plum";
+  if (rating < 19999) return "purple";
 
-  if (rating < 24999) return "violet";
+  if (rating < 24999) return "pink";
 
-  if (rating < 29999) return "crimson";
+  if (rating < 29999) return "red";
 
   return "amber";
 }
